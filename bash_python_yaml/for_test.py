c1b0e4b0f353bb6205aaf765c36e41002ef8d0a6
mfonarev <fonicemiha@mail.ru>
2022-10-11 11:16:25 +0300

    dz complete

--- bash_python_yaml/for_test.py
+++ bash_python_yaml/for_test.py
@@ -1,8 +1,8 @@
 import socket
+import json
+import yaml
 
 url_list_cache = {'drive.google.com': '', 'mail.google.com': '', 'google.com': ''}
-
-
 while True:
     for i in url_list_cache.items():
         ip = socket.gethostbyname(i[0])
@@ -11,3 +11,7 @@ while True:
             url_list_cache.update({i[0]: ip})
         else:
             print(f'{i[0]} - {i[1]}')
+        with open(f'{i[0]}.json', 'w') as json_out:
+            json_out.write(json.dumps({f'{i[0]}': f'{ip}'}))
+        with open(f'{i[0]}.yaml', 'w') as yaml_out:
+            yaml.dump([{f'{i[0]}': f'{ip}'}], yaml_out)
